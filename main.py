8cdf78dcaa1ddb556511f20b04cfddf06017c85a
Matt Bremmers <bremmdev@gmail.com>
2023-10-14 13:45:30 +0200

    remove unused import

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, render_template_string, request, redirect
-from data import expenses
 from models.category import Category, CategoryExistsError, CategoryNotFoundError
 from models.expenses import Expense
 from models.budget import Budget
